a87dd8229b09ec9aadc6e8f2aeec83c27c6eb2a9
Hafizh <Hafizh@gmail.com>
2023-12-21 13:56:37 +0700

    Add model ayat

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,63 +1,23 @@
 import 'dart:convert';
 
-import 'package:alquran/app/data/models/detail_surah.dart';
-import 'package:alquran/app/data/models/surah.dart';
+import 'package:alquran/app/data/models/ayat.dart';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  // Uri url = Uri.parse('https://api.quran.gading.dev/surah/');
-  // var res = await http.get(url);
+  var res = await http.get(
+    Uri.parse('https://api.quran.gading.dev/surah/108/1'),
+  );
+  Map<String, dynamic> data = json.decode(res.body)['data'];
+  Map<String, dynamic> dataToModel = {
+    'number': data['number'],
+    'meta': data['meta'],
+    'text': data['text'],
+    'translation': data['translation'],
+    'audio': data['audio'],
+    'tafsir': data['tafsir'],
+  };
 
-  // var data = (json.decode(res.body) as Map<String, dynamic>)['data'];
-
-  // // 1-114 -> index ke-113 = An nas
-  // //print(data[113]['number']);
-
-  // // data dari API(raw data) => Model(yang sudah dibuat)
-  // Surah surahAnnas = Surah.fromJson(data[113]);
-  // // print(surahAnnas.name);
-  // // print('===========');
-  // // print(surahAnnas.number);
-  // // print('===========');
-  // // print(surahAnnas.numberOfVerses);
-  // // print('===========');
-  // // print(surahAnnas.revelation);
-  // // print('===========');
-  // // print(surahAnnas.sequence);
-  // // print('===========');
-  // // print(surahAnnas.tafsir);
-
-  // // Ini coba masuk ke nested model
-  // // print(surahAnnas.number);
-
-  // Uri urlAnnas =
-  //     Uri.parse('https://api.quran.gading.dev/surah/${surahAnnas.number}');
-  // var resAnnas = await http.get(urlAnnas);
-
-  // //print(resAnnas.body);
-
-  // Map<String, dynamic> dataAnnas =
-  //     (json.decode(resAnnas.body) as Map<String, dynamic>)['data'];
-
-  // // print(dataAnnas);
-
-  // // data dari API(raw data) => Model(yang sudah dibuat)
-  // DetailSurah annas = DetailSurah.fromJson(dataAnnas);
-
-  // print(annas.verses![0].text!.arab);
-
-  Future<DetailSurah> getDetailSurah(String id) async {
-    Uri url = Uri.parse('https://api.quran.gading.dev/surah/$id');
-
-    var res = await http.get(url);
-
-    Map<String, dynamic> data =
-        (json.decode(res.body) as Map<String, dynamic>)['data'];
-    DetailSurah test = DetailSurah.fromJson(data);
-    print(test);
-
-    return DetailSurah.fromJson(data);
-  }
-
-  await getDetailSurah(1.toString());
+  // Convert Map -> Model Ayat
+  Ayat ayat = Ayat.fromJson(dataToModel);
+  print(ayat.tafsir.id.short);
 }
